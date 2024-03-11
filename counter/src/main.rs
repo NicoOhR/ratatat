5eb905d68fd1cd2a951bbe6e613f211ec8af4316
NicoOhR <nimoohayonr@gmail.com>
2024-03-10 21:35:41 -0600

    counter

--- counter/src/main.rs
+++ counter/src/main.rs
@@ -29,7 +29,34 @@ impl App {
     }
 
     fn handle_events(&mut self) -> io::Result<()>{
-        todo!()
+        match event::read()?{
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent){
+        match key_event.code{
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self){
+        self.exit = true;
+    }
+
+    fn decrement_counter(&mut self){
+        self.counter -= 1;
+    }
+
+    fn increment_counter(&mut self){
+        self.counter += 1;
     }
 }
 
