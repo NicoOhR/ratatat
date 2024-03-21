6f10eed2a6b105079c4c71c994afdaafada0df64
NicoOhR <nimoohayonr@gmail.com>
2024-03-20 22:29:37 -0500

    adding main deps, some boiler

--- cinetrack/src/main.rs
+++ cinetrack/src/main.rs
@@ -1,4 +1,38 @@
+use std::{error::Error, io};
+
+use crossterm::{
+    event::{
+        self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode,
+        KeyEventKind,
+    },
+    execute,
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+};
+use ratatui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
 
 mod app;
+//mod ui;
+use crate::{
+    app::{App, CurrentScreen, AddingMovie},
+    //ui::ui,
+};
 
-fn main() {}
+fn main() -> Result<(), Box<dyn Error>>{
+    enable_raw_mode()?;
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut app = App::new();
+
+    Ok(())
+
+}
